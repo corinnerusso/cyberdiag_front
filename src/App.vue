d965cf3655183da6644911956e3be3946fba80b0
Corinne Russo <corinne.russo@cyberens.fr>
2020-03-10 16:57:21 +0100

    initialize navbar

--- src/App.vue
+++ src/App.vue
@@ -3,39 +3,57 @@
     <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
+          :src="require('./assets/security.svg')"
+          class="my-3"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          height="200"
           transition="scale-transition"
           width="40"
         />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <span class="create">CYBERDIAG</span>
       </div>
 
       <v-spacer></v-spacer>
 
       <v-btn
+        class="navbar-button"
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <div class="d-flex align-center">
+          <i class="material-icons">
+            create
+          </i>
+        </div>
+        <span class="mr-2">Créer un nouveau questionnaire</span>
       </v-btn>
+      <div>
+        <i class="material-icons" color="black">
+          perm_identity
+        </i>
+      </div>
+      <v-col cols="12" sm="1">
+        <v-overflow-btn
+          class="my-2"
+          :items="dropdown_choice"
+          target="#dropdown-example"
+        ></v-overflow-btn>
+      </v-col>
     </v-app-bar>
 
     <v-content>
       <HelloWorld />
     </v-content>
+    <div id="app">
+      <v-app id="inspire">
+        <v-footer padless>
+          <v-col class="text-center" cols="12">
+            {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+          </v-col>
+        </v-footer>
+      </v-app>
+    </div>
   </v-app>
 </template>
 
@@ -50,7 +68,23 @@ export default {
   },
 
   data: () => ({
-    //
+    dropdown_choice: [{ text: "Mon compte" }, { text: "Deconnexion" }]
   })
 };
 </script>
+
+<style scoped>
+.v-application .primary {
+  background-color: #165874 !important;
+}
+
+.create {
+  font-size: 1.7em;
+  margin-left: 10%;
+}
+
+.navbar-button {
+  background-color: #38718a;
+  box-shadow: 0 4px 25px 0 rgba(0, 0, 0, 0.1);
+}
+</style>
