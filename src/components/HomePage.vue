25189b2adabec460269d11dbd39bc01648ba5637
Corinne Russo <corinne.russo@cyberens.fr>
2020-03-12 17:17:51 +0100

    change profile picture and dropdown button

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,23 +1,14 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="surveys"
-    sort-by="client"
-    class="elevation-1"
-  >
+  <v-data-table :headers="headers" :items="surveys" sort-by="client" class="elevation-1">
     <template v-slot:top>
       <p>BARRE DE RECHERCHE</p>
       <v-toolbar color="#4CA3BB">
-        <v-toolbar-title color="white !important"
-          >MES QUESTIONNAIRES</v-toolbar-title
-        >
+        <v-toolbar-title color="white !important">MES QUESTIONNAIRES</v-toolbar-title>
         <v-divider class="mx-4" inset vertical></v-divider>
         <v-spacer></v-spacer>
         <v-dialog v-model="dialog" max-width="500px">
           <template v-slot:activator="{ on }">
-            <v-btn color="#40778f" dark class="mb-2" v-on="on"
-              >Nouveau questionnaire</v-btn
-            >
+            <v-btn color="#40778f" dark class="mb-2" v-on="on">Nouveau questionnaire</v-btn>
           </template>
 
           <v-card>
@@ -29,28 +20,16 @@
               <v-container>
                 <v-row>
                   <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.name"
-                      label="Survey"
-                    ></v-text-field>
+                    <v-text-field v-model="editedItem.name" label="Survey"></v-text-field>
                   </v-col>
                   <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.client"
-                      label="client"
-                    ></v-text-field>
+                    <v-text-field v-model="editedItem.client" label="client"></v-text-field>
                   </v-col>
                   <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.created"
-                      label="created"
-                    ></v-text-field>
+                    <v-text-field v-model="editedItem.created" label="created"></v-text-field>
                   </v-col>
                   <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.type"
-                      label="type"
-                    ></v-text-field>
+                    <v-text-field v-model="editedItem.type" label="type"></v-text-field>
                   </v-col>
                 </v-row>
               </v-container>
@@ -66,12 +45,15 @@
       </v-toolbar>
     </template>
     <template v-slot:item.actions="{ item }">
-      <v-icon small class="mr-2" @click="editItem(item)">
-        mdi-pencil
-      </v-icon>
-      <v-icon small @click="deleteItem(item)">
-        mdi-delete
-      </v-icon>
+      <v-icon
+        small
+        class="mr-2"
+        @click="editItem(item)"
+        title="Modifier le questionnaire"
+      >mdi-pencil</v-icon>
+      <v-icon small @click="deleteItem(item)" title="Supprimer">mdi-delete</v-icon>
+      <v-icon small @click="deleteItem(item)" title="Aller au questionnaire">arrow_right_alt</v-icon>
+      <v-icon small @click="deleteItem(item)" title="Voir les résultats">trending_up</v-icon>
     </template>
     <template v-slot:no-data>
       <v-btn color="primary" @click="initialize">Reset</v-btn>
@@ -102,15 +84,15 @@ export default {
     editedIndex: -1,
     editedItem: {
       name: "",
-      client: 0,
-      created: 0,
-      type: 0
+      client: "",
+      type: "",
+      created: ""
     },
     defaultItem: {
       name: "",
-      client: 0,
-      created: 0,
-      type: 0
+      client: "",
+      type: "",
+      created: ""
     }
   }),
 
@@ -174,4 +156,8 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.v-icon.v-icon.v-icon--link {
+  margin: 4px;
+}
+</style>
