761fc5c6b91f24d954c9dc9900de42a00f06a041
Corinne Russo <corinne.russo@cyberens.fr>
2020-03-13 14:05:06 +0100

    login page

---
+++ src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div style="margin-top:100px" class="home">
+    <LoginPage />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import LoginPage from "@/components/LoginPage.vue";
+
+export default {
+  name: "Login",
+  components: {
+    LoginPage
+  }
+};
+</script>
+
+<style scoped>
+.home {
+  padding-left: 10%;
+  padding-right: 10%;
+}
+</style>
